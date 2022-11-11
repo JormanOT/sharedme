5f61b4ff6e0c636d20728470ec84ee61750138af
JormanOT <jormanortega34@gmail.com>
2022-11-11 23:10:18 +0100

    Version 1

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import NavBar from './Navbar'
+import SideBar from './SideBar'
+import Feed from './Feed'
+import PinDetail from './PinDetail'
+import UserProfile from './UserProfile'
+import CreatePin from './CreatePin'
+import Search from './Search'
+import Login from './Login'
+import MasonryLayout from './MasonryLayout'
+
+export { NavBar, SideBar, Feed, PinDetail, UserProfile, CreatePin, Search, Login, MasonryLayout }
